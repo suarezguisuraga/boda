90bae5051caf50c39472150ea70cd47f16ae3869
Suarez Guisuraga <angelsuarez@kpmg.com>
2018-02-23 19:15:19 +0100

    Update with securty checks

--- public/js/securitycheck.js
+++
@@ -1,5 +0,0 @@
-<!-- Return to security check -->
-var ref = document.referrer;
-if(!ref.match(/^https?:\/\/([^\/]+\.)?suarezguisuraga\.com(\/|$)/i)){
-	window.location = "http://www.suarezguisuraga.com";
-}
